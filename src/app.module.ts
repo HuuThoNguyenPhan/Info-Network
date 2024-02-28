917d463b5f1f67ec73a6717371e534ea27ea3267
Nguyễn Phan Hữu Thọ <103346384+HuuThoNguyenPhan@users.noreply.github.com>
2024-02-28 14:34:27 +0700

    feat(dev-env): add prettier to git hooks

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,25 +6,25 @@ import { database_config } from './configs/configuration.config';
 import * as Joi from 'joi';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        NODE_ENV: Joi.string()
-          .valid('development', 'production', 'test', 'provision', 'staging')
-          .default('development'),
-        PORT: Joi.number().port().required(),
-        DATABASE_PORT: Joi.number().port().required(),
-      }),
-      validationOptions: {
-        abortEarly: false,
-      },
-      load: [database_config],
-      isGlobal: true,
-      expandVariables: true,
-      envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env',
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			validationSchema: Joi.object({
+				NODE_ENV: Joi.string()
+					.valid('development', 'production', 'test', 'provision', 'staging')
+					.default('development'),
+				PORT: Joi.number().port().required(),
+				DATABASE_PORT: Joi.number().port().required(),
+			}),
+			validationOptions: {
+				abortEarly: false,
+			},
+			load: [database_config],
+			isGlobal: true,
+			expandVariables: true,
+			envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env',
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
--- src/configs/configuration.config.ts
+++ src/configs/configuration.config.ts
@@ -1,17 +1,17 @@
 export interface DatabaseConfig {
-    host: string;
-    port: number;
-    username: string;
-    password: string;
-    dbname: string;
+	host: string;
+	port: number;
+	username: string;
+	password: string;
+	dbname: string;
 }
 
 export const database_config = () => ({
-    database: {
-        host: process.env.DATABASE_HOST,
-        port: parseInt(process.env.DATABASE_PORT, 10),
-        urername: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        dbname: process.env.DATABASE
-    },
+	database: {
+		host: process.env.DATABASE_HOST,
+		port: parseInt(process.env.DATABASE_PORT, 10),
+		urername: process.env.DATABASE_USER,
+		password: process.env.DATABASE_PASSWORD,
+		dbname: process.env.DATABASE,
+	},
 });
